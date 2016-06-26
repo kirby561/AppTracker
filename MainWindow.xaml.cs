7ffe3c6f382d280f010d1931066e0f0ca3e2e013
Jim Raynor <fudgemutator@gmail.com>
2016-06-26 14:17:21 -0400

    Added ascending/descending column sorting by clicking on the columns.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,39 +25,40 @@ namespace AppTracker {
         private double _timePeriod = 5000;
         private List<ProcessEntry> _displayedList = new List<ProcessEntry>();
 
+        // Keep track of the sorted column so we can switch it
+        private String _currentSortedColumnName = null;
+        private bool _sortedColumnIsAscending = true;
+
         public MainWindow() {
             InitializeComponent();
+
+            // Create timer
             _checkProcessesTimer = new Timer();
             _checkProcessesTimer.AutoReset = true;
             _checkProcessesTimer.Interval = _timePeriod;
             _checkProcessesTimer.Elapsed += OnCheckProcessesTimerElapsed;
             _checkProcessesTimer.Start();
+
+            // Set the list item source
             _processListView.ItemsSource = _displayedList;
+
+            // Hook up column header events for sorting
+            InitializeColumnEvents();
         }
 
-        private void OnCheckProcessesTimerElapsed(object sender, ElapsedEventArgs e) {
-            Process[] processes = Process.GetProcesses();
-            foreach (Process process in processes) {
-                bool wasNew = _timeTracker.AddTime(process.ProcessName, _timePeriod);
-
-                if (wasNew) {
-                    _displayedList.Add(_timeTracker.GetEntry(process.ProcessName));
-                }
+        private void InitializeColumnEvents() {
+            GridView itemsView = _processListView.View as GridView;
+            if (itemsView == null) {
+                throw new Exception("Expected a GridView so we can resize the columns.");
             }
-            App.Current.Dispatcher.Invoke(() => {
-                _processListView.Items.Refresh();
-            });
+
+            foreach (GridViewColumn column in itemsView.Columns) {
+                GridViewColumnHeader header = column.Header as GridViewColumnHeader;
+                header.Click += OnColumnHeaderClicked;
+            }
         }
 
-        private void OnWindowLoaded(object sender, RoutedEventArgs e) {
-            SizeLastColumn();
-        }
-
-        private void OnProcessListViewSizeChanged(object sender, SizeChangedEventArgs e) {
-            SizeLastColumn();
-        }
-
-        void SizeLastColumn() {
+        private void SizeLastColumn() {
             GridView itemsView = _processListView.View as GridView;
             if (itemsView == null) {
                 throw new Exception("Expected a GridView so we can resize the columns.");
@@ -80,6 +81,51 @@ namespace AppTracker {
                 }
                 index++;
             }
+        }
+
+        private void OnCheckProcessesTimerElapsed(object sender, ElapsedEventArgs e) {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes) {
+                bool wasNew = _timeTracker.AddTime(process.ProcessName, _timePeriod);
+
+                if (wasNew) {
+                    _displayedList.Add(_timeTracker.GetEntry(process.ProcessName));
+                }
+            }
+
+            App.Current.Dispatcher.Invoke(() => {
+                _processListView.Items.Refresh();
+            });
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e) {
+            SizeLastColumn();
+        }
+
+        private void OnColumnHeaderClicked(object sender, RoutedEventArgs e) {
+            GridViewColumnHeader header = sender as GridViewColumnHeader;
+            String headerName = header.Content as String;
+
+            Func<ProcessEntry, object> sortItemAccessor = null;
+            if ("Process".Equals(headerName))
+                sortItemAccessor = (entry => entry.Name);
+            else if ("Time (Hours)".Equals(headerName))
+                sortItemAccessor = (entry => entry.Time);
+
+            if (headerName.Equals(_currentSortedColumnName) && _sortedColumnIsAscending) {
+                _displayedList = _displayedList.OrderByDescending(sortItemAccessor).ToList();
+                _sortedColumnIsAscending = false;
+            } else {
+                _displayedList = _displayedList.OrderBy(sortItemAccessor).ToList();
+                _sortedColumnIsAscending = true;
+            }
+            _processListView.ItemsSource = _displayedList;
+            _currentSortedColumnName = headerName;
+        }
+
+        private void OnProcessListViewSizeChanged(object sender, SizeChangedEventArgs e) {
+            SizeLastColumn();
         }
     }
 }
